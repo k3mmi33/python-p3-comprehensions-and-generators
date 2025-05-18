6d1a5e30e34cf17cbae988975f6a7c7114c0fde7
Natasha Onsongo <kemmienatasha@gmail.com>
2025-05-19 00:59:47 +0300

    completed

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    # Return a list of even numbers from num_list using a list comprehension
+    return [num for num in num_list if num % 2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    # Return a list with an exclamation mark added to each sentence in sentence_list using a list comprehension
+    return [sentence + "!" for sentence in sentence_list]
